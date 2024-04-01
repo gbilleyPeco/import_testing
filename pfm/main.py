b8d6189091a3cf0f05d08726f749c9a42ed1182f
Graham Billey <gbilley@pecopallet.com>
2024-04-01 14:35:10 -0400

    app.key imports with ROOT in sys path

--- pfm/main.py
+++ pfm/main.py
@@ -6,15 +6,15 @@ print('\n\n------------------------------\n\n')
 print('PATH Section\n')
 import sys
 import os
-print('Before:\n')
-print(sys.path)
-print('\n\n')
+# print('Before:\n')
+# print(sys.path)
+# print('\n\n')
 ROOT = os.path.abspath(os.path.join('..'))
 if ROOT not in sys.path:
     sys.path.append(ROOT)
 
-print('After:\n')
-print(sys.path)
+# print('After:\n')
+# print(sys.path)
 
 #%% Try the import.
 print('\n\n------------------------------\n\n')
@@ -29,7 +29,11 @@ from utils import parse_config
 # ROOT Config
 cfg_path = os.path.join(ROOT, 'config.toml')
 root_cfg = parse_config(cfg_path)
-print(f'ROOT Config: {root_cfg}\n\n')
+# print(f'ROOT Config: {root_cfg}\n\n')
 # PFM Config
 pfm_cfg = parse_config('config.toml')
-print(f'PFM Config: {pfm_cfg}\n\n')
+# print(f'PFM Config: {pfm_cfg}\n\n')
+
+#%% Figure out how to get the utils file to read from app.key
+from utils import read_app_key
+read_app_key()
--- utils.py
+++ utils.py
@@ -1,4 +1,15 @@
 import tomllib
+import os
+import sys
+
+# The project ROOT gets added as the final element of the sys.path variable in main.py.
+# The project ROOT is needed to tell the read_app_key method where the file 'app.key' exists.
+# This is becasue the working directory is determined by the location of the batch file. 
+# Accessing the root directory this way seems sort of "hacky", but simplifies the code and 
+# makes it so I don't have to move my batch files into the root directory, or change the 
+# working directory explicitly.
+
+ROOT = sys.path[-1]
 
 def utility_function():
     print('I am a utility function!')
@@ -16,3 +27,10 @@ def parse_config(config_path):
     with open(config_path, "rb") as f:
         cfg = tomllib.load(f)
     return cfg
+
+def read_app_key():
+    print('INSIDE read_app_key method...')
+    path_to_app_key = os.path.join(ROOT, 'app.key')
+    with open(path_to_app_key, 'r') as file:
+        USER_NAME, APP_KEY = file.read().split('\n')
+        print(f'User Name: {USER_NAME}, App Key: {APP_KEY}')
